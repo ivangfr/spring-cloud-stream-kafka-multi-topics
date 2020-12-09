63a51cbd33f20a05518e8a1aa1b10af1ba23436c
Ivan Franchin <ivan.franchin@takeaway.com>
2020-12-09 08:53:42 -0300

    implement NewsProducer using functional model programming of spring-cloud-stream

--- spring-cloud-stream/producer-cloud-stream/src/main/java/com/mycompany/producercloudstream/kafka/NewsProducer.java
+++ spring-cloud-stream/producer-cloud-stream/src/main/java/com/mycompany/producercloudstream/kafka/NewsProducer.java
@@ -4,8 +4,7 @@ import com.mycompany.producercloudstream.domain.News;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
@@ -13,13 +12,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-@EnableBinding(Source.class)
 public class NewsProducer {
 
-    @Value("${spring.cloud.stream.bindings.output.destination}")
+    @Value("${spring.cloud.stream.bindings.stream-out-0.destination}")
     private String kafkaTopic;
 
-    private final Source source;
+    private final StreamBridge streamBridge;
 
     public void send(News news) {
         log.info("Sending News '{}' to topic '{}'", news, kafkaTopic);
@@ -27,6 +25,6 @@ public class NewsProducer {
         Message<News> message = MessageBuilder.withPayload(news)
                 .setHeader("partitionKey", news.getId())
                 .build();
-        source.output().send(message);
+        streamBridge.send("stream-out-0", message);
     }
 }
